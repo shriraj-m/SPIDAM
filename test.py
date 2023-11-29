b669c8748e5d16727805d35294c705e2453d4e10
Shriraj Mandulapalli <smandulapalli@outlook.com>
2023-11-29 14:24:50 -0500

    added functionality for wav files carrying different channels counts. included two new functions

--- test.py
+++ test.py
@@ -1,29 +1,36 @@
-
-import tkinter as tk
+# import tkinter as tk
 import numpy as np
-import wave
-import sys
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
-import scipy.io
 
 
-wav_filename = ("16bit4chan.wav")
+def define_channel_count():
+    if len(data.shape) == 1:
+        return 1
+    else:
+        return data.shape[1]
+
+
+def plot_labeling(channel_count):
+    if channel_count == 1:
+        plt.plot(time, data, label="Channel 1")
+    elif channel_count > 1:
+        for x in range(channel_count):
+            label_title = f'Channel {x+1}'
+            plt.plot(time, data[:, x], label=label_title)
+
+
+wav_filename = "16bit4chan.wav"
 samplerate, data = wavfile.read(wav_filename)
-print(data.shape[len(data.shape) - 1])
-print(samplerate)
 length = data.shape[0] / samplerate
-print(length)
 
 # 1chan: 101430, 44100, 2.3s
 # 2chan: 2, 44100, 0.4658s
 # 4chan: 4, 44100, 2.3s
 
 time = np.linspace(0, length, data.shape[0])
-print(time)
-plt.plot(time, data, label="First Channel")
-'''
-plt.plot(time, data[:, 1], label="Second Channel")  
+plot_labeling(define_channel_count())
+'''plt.plot(time, data[:, 1], label="Second Channel")
 plt.plot(time, data[:, 2], label="Third channel")
 plt.plot(time, data[:, 3], label="Fourth Channel")'''
 plt.legend()
@@ -31,34 +38,3 @@ plt.xlabel("Time [s]")
 plt.ylabel("Amplitude")
 plt.show()
 
-
-'''
-import tkinter as tk
-import numpy as np
-import wave
-import sys
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
-import scipy.io
-
-
-wav_filename = ("16bit4chan.wav")
-samplerate, data = wavfile.read(wav_filename)
-print(data.shape[len(data.shape) - 1])
-print(samplerate)
-length = data.shape[0] / samplerate
-print(length)
-
-# 1chan: 101430, 44100, 2.3s
-# 2chan: 2, 44100, 0.4658s
-# 4chan: 4, 44100, 2.3s
-
-time = np.linspace(0, length, data.shape[0])
-print(time)
-plt.plot(time, data, label="Left channel")
-# plt.plot(time, data[:, 1], label="Right channel")
-plt.legend()
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()
-'''
